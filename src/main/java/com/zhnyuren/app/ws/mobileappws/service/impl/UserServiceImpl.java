dcd55fd68da9f14302ec5c6d07cd733b3194e2e5
Yuren Zhang <zhnyuren@outlook.com>
2020-12-09 23:45:33 +0800

    Handle user duplication and user public uuid generation

--- src/main/java/com/zhnyuren/app/ws/mobileappws/service/impl/UserServiceImpl.java
+++ src/main/java/com/zhnyuren/app/ws/mobileappws/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.zhnyuren.app.ws.mobileappws.UserRepository;
 import com.zhnyuren.app.ws.mobileappws.io.entity.UserEntity;
 import com.zhnyuren.app.ws.mobileappws.service.UserService;
 import com.zhnyuren.app.ws.mobileappws.shared.dto.UserDto;
+import com.zhnyuren.app.ws.mobileappws.shared.dto.Utils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,20 +12,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private static final int USER_ID_LENGTH = 30;
+
     private final UserRepository userRepository;
 
+    private final Utils utils;
+
     @Autowired
-    public UserServiceImpl(final UserRepository userRepository) {
+    public UserServiceImpl(final UserRepository userRepository,
+                           final Utils utils) {
         this.userRepository = userRepository;
+        this.utils = utils;
     }
 
     @Override
     public UserDto createUser(UserDto userDto) {
+        if (null != userRepository.findByEmail(userDto.getEmail())) {
+            throw new RuntimeException("Record already exists");
+        }
+
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(userDto, userEntity);
 
         userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("testUserId");
+        userEntity.setUserId(utils.generateUserId(USER_ID_LENGTH));
 
         UserEntity storedUserDetails = userRepository.save(userEntity);
 
--- src/main/java/com/zhnyuren/app/ws/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/zhnyuren/app/ws/mobileappws/ui/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
